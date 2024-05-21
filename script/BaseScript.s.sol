3ddf124d525c455426916ec5f407c29e9a32e936
Benjamin <benjaminxh+github@gmail.com>
2024-05-21 07:34:19 +0200

    add eOracle, minor refactor

---
+++ script/BaseScript.s.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { Script } from "forge-std/Script.sol";
+import { BN254 } from "eigenlayer-middleware/src/libraries/BN254.sol";
+import { IBLSApkRegistry } from "eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
+import { ISignatureUtils } from "eigenlayer-contracts/src/contracts/interfaces/ISignatureUtils.sol";
+import { IAVSDirectory } from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+
+
+contract BaseScript is Script {
+    using BN254 for BN254.G1Point;
+    using Strings for uint256;
+
+    // Generates bls pubkey params from a private key
+    function _generateBlsPubkeyParams(uint256 privKey)
+        internal
+        returns (IBLSApkRegistry.PubkeyRegistrationParams memory)
+    {
+        IBLSApkRegistry.PubkeyRegistrationParams memory pubkey;
+        pubkey.pubkeyG1 = BN254.generatorG1().scalar_mul(privKey);
+        pubkey.pubkeyG2 = _mulGo(privKey);
+        return pubkey;
+    }
+
+    function _mulGo(uint256 x) internal returns (BN254.G2Point memory g2Point) {
+        string[] memory inputs = new string[](3);
+        // inputs[0] = "./go2mul-mac"; // lib/eigenlayer-middleware/test/ffi/go/g2mul.go binary
+        // inputs[0] = "./go2mul-arm64"; // lib/eigenlayer-middleware/test/ffi/go/g2mul.go binary
+        inputs[0] = "./go2mul"; // lib/eigenlayer-middleware/test/ffi/go/g2mul.go binary
+        inputs[1] = x.toString();
+
+        inputs[2] = "1";
+        bytes memory res = vm.ffi(inputs);
+        g2Point.X[1] = abi.decode(res, (uint256));
+
+        inputs[2] = "2";
+        res = vm.ffi(inputs);
+        g2Point.X[0] = abi.decode(res, (uint256));
+
+        inputs[2] = "3";
+        res = vm.ffi(inputs);
+        g2Point.Y[1] = abi.decode(res, (uint256));
+
+        inputs[2] = "4";
+        res = vm.ffi(inputs);
+        g2Point.Y[0] = abi.decode(res, (uint256));
+    }
+
+    /**
+     * @notice internal function for calculating a signature from the operator corresponding to `_operatorPrivateKey`, delegating them to
+     * the `operator`, and expiring at `expiry`.
+     */
+    function _getOperatorSignature(
+        uint256 _operatorPrivateKey,
+        address operator,
+        address avs,
+        bytes32 salt,
+        uint256 expiry
+    ) internal view returns (bytes32 digestHash, ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature) {
+        operatorSignature.expiry = expiry;
+        operatorSignature.salt = salt;
+        {
+            digestHash = IAVSDirectory(_getAVSDirectoryAddress()).calculateOperatorAVSRegistrationDigestHash(
+                operator, avs, salt, expiry
+            );
+            (uint8 v, bytes32 r, bytes32 s) = vm.sign(_operatorPrivateKey, digestHash);
+            operatorSignature.signature = abi.encodePacked(r, s, v);
+        }
+        return (digestHash, operatorSignature);
+    }
+
+    function _getAVSDirectoryAddress() internal view returns(address) {
+        if (block.chainid == 1) {
+            return 0x135DDa560e946695d6f155dACaFC6f1F25C1F5AF;
+        }
+
+        if (block.chainid == 17000) {
+            return 0x055733000064333CaDDbC92763c58BF0192fFeBf;
+        }
+
+        revert("Invalid chainId");
+    }
+}
